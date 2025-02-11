f964aeb4c452f9ce9f871e5ddbaee0c70e2003b4
LoKhyeHe <89993223+LoKhyeHe@users.noreply.github.com>
2025-02-11 20:39:39 +0800

    By Bervin

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import os
 import streamlit as st
 from google import genai
 
-st.title("Welcome to SUTD AI Interest Group Streamlit x Gemini Workshop\nBy Bervin")
+st.title("Welcome to SUTD AI Interest Group Streamlit x Gemini Workshop\nBy Smervin")
